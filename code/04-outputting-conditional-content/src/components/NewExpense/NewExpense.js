999c3aa01b73838e7a5101afb1abc08e276ab981
Tim Matyas <matyastr@gmail.com>
2021-08-12 08:02:00 -0400

    chore: sect 5 assign 4

--- code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
+++ code/04-outputting-conditional-content/src/components/NewExpense/NewExpense.js
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
-      id: Math.random().toString()
+      id: Math.random().toString(),
     };
     props.onAddExpense(expenseData);
   };
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
-  );
+  const [showNewExpense, setShowNewExpense] = useState(false);
+
+  const toggleNewExpenseHandler = () => {
+    setShowNewExpense((prevState) => {return !prevState});
+  };
+
+  let expenseBlockToShow = null;
+
+  if (showNewExpense) {
+    expenseBlockToShow = (
+      <ExpenseForm
+        onSaveExpenseData={saveExpenseDataHandler}
+        onClose={toggleNewExpenseHandler}
+      />
+    );
+  } else {
+    expenseBlockToShow = (
+      <button onClick={toggleNewExpenseHandler} type="submit">
+        Add New Expense
+      </button>
+    );
+  }
+
+  return <div className="new-expense">{expenseBlockToShow}</div>;
 };
 
 export default NewExpense;
